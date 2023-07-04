6b9a24346623fdf4b646879d55afd340d4c9ca3d
z2358 <zhoul2358@dtdream.com>
2023-07-04 14:07:54 +0800

    2023-07-04 first commit

---
+++ quiz/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;/**
+* @author DiDi
+* @description
+* @date ${DATE} ${TIME}
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
