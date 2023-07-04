bd4d47ecd508ad876e250c8c0c941a1ab504be98
z2358 <zhoul2358@dtdream.com>
2023-07-04 13:50:55 +0800

    2023-07-04 first commit

---
+++ quiz/src/main/java/com/example/quiz/Sqrt.java
@@ -0,0 +1,43 @@
+
+/*
+ * ------------------------------------------------------------------
+ * Copyright @ 2023 Hangzhou DtDream Technology Co.,Ltd. All rights reserved.
+ * ------------------------------------------------------------------
+ *       Product: DataRiver
+ *   Module Name:
+ *  Date Created: 2023/7/4
+ *   Description:
+ * ------------------------------------------------------------------
+ * Modification History
+ * DATE            Name           Description
+ * ------------------------------------------------------------------
+ * 2023/7/4       底迪 z2358
+ * ------------------------------------------------------------------
+ */
+package com.example.quiz;
+
+/**
+ * @author DiDi
+ * @description 实现开方
+ * @date 2023/7/4 13:49
+ */
+public class Sqrt {
+
+    /**
+     * 解法1
+     */
+    private static int sort1() {
+        return 0;
+    }
+
+    /**
+     * 解法2
+     */
+    private static int sort2() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
